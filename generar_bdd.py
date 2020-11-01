d90fb9ee3df1305f6e46a8b225eb4479adc06dab
Juan Cruz Colazo <juan@hlx.co>
2020-11-01 19:13:21 -0300

    Se agregan comentarios

--- generar_bdd.py
+++ generar_bdd.py
@@ -10,11 +10,11 @@ access = CreateObject('Access.Application')
 DBEngine = access.DBEngine
 while(True):
     try:
-        db = DBEngine.OpenDatabase(os.getcwd()+'/telefonos.mdb')
+        db = DBEngine.OpenDatabase(os.getcwd()+'/telefonos.mdb') #Se busca si existe la bdd en la carpeta
         print(colored('Base de datos encontrada, abriendo','green'))
     except:
         print(colored('Base de datos no encontrada, creando','yellow'))
-        db = DBEngine.CreateDatabase(os.getcwd()+'/telefonos.mdb', Access.DB_LANG_GENERAL)
+        db = DBEngine.CreateDatabase(os.getcwd()+'/telefonos.mdb', Access.DB_LANG_GENERAL) #Si no se encuentra, se crea.
         db.Close()
         continue
     break
@@ -24,15 +24,15 @@ def crear_tablas(): #Funcion para crear las 10 tablas
         for i in range(1,11):
             db.Execute("create table telefonos"+str(i)+"(id int,numero NUMERIC)")
     except:
-        print(colored('Tablas ya creadas, saliendo','red'))
+        print(colored('Tablas ya creadas, saliendo','red')) #Si las tablas ya estan creadas, se termina el script
         sys.exit()
-def cargar_datos():
+def cargar_datos(): #Funcion para cargar las tablas
     db.BeginTrans()
-    lista_telefonos= random.sample(range(54911000000,(54911000000+70000)),70000)
+    lista_telefonos= random.sample(range(54911000000,(54911000000+70000)),70000) #Con la funcion random se generan los numeros de telefono
     cont=0
     cont2=1
     z=1
-    for l in tqdm(lista_telefonos,colour='green',desc=colored('Cargando datos','green')):
+    for l in tqdm(lista_telefonos,colour='green',desc=colored('Cargando datos','green')): #tqdm es una funcion para mostrar la barra de progreso en la terminal
         if cont==7000:
             cont=0
             z+=1
